877aa550316efc73500139d44afced00153b3b14
Michela <micmif@gmail.com>
2019-08-30 09:05:41 +0200

    Login and Register Pages Set

--- Bud/src/app/app.module.ts
+++ Bud/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -13,7 +14,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
@@ -23,12 +25,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
---
+++ Bud/src/pages/login/login.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { RegisterPage } from '../register/register';
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  email:string;
+  password:string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  login(){
+    console.log("Username: "+this.email);
+    console.log("Password: "+ this.password);
+
+    this.navCtrl.push(HomePage);
+  }
+
+  goRegister(){
+   this.navCtrl.push(RegisterPage);
+  }
+}
